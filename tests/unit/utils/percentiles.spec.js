2f74a4f327df4a2eed8a927df61b6429def94baf
Ubald <ubald.de-bellabre@external.total.com>
2021-08-21 16:27:56 +0200

    Imported function to compute percentiles from an array

---
+++ tests/unit/utils/percentiles.spec.js
@@ -0,0 +1,63 @@
+import {expect} from "chai"
+
+function computePercentile(array, percentile) {
+	const sortedArray = [...array].sort()
+	const percentileIndex = findPercentileIndex(percentile, array)
+	return sortedArray[percentileIndex] ?? 0
+}
+
+function findPercentileIndex(percentile, array) {
+	const percentileIndex = (percentile / 100) * (array.length - 1)
+	if (Number.isInteger(percentileIndex)) {
+		return percentileIndex
+	}
+	return Math.floor(percentileIndex) + 1
+}
+
+describe('computePercentile', () => {
+	[0, 50, 100].forEach(percentile => {
+		it(`returns 0 for ${percentile} when given an empty arrays`, () => {
+			const actual = computePercentile([], percentile)
+
+			expect(actual).to.equal(0)
+		})
+	})
+
+	it('computes maximum for 100 percentile', () => {
+		const percentile = computePercentile([1, 2, 3], 100)
+
+		expect(percentile).to.equal(3)
+	})
+
+	it('computes minimum for 0 percentile', () => {
+		const percentile = computePercentile([3, 2, 1], 0)
+
+		expect(percentile).to.equal(1)
+	})
+
+	it('computes median for 50 percentile', () => {
+		const percentile = computePercentile([3, 2, 1], 50)
+
+		expect(percentile).to.equal(2)
+	})
+
+	it('computes median for arrays with even number of elements', () => {
+		const percentile = computePercentile([1, 2, 3, 4], 50)
+
+		expect(percentile).to.equal(3)
+	})
+
+	it('works with unsorted arrays', () => {
+		const percentile = computePercentile([3, 2, 1], 100)
+
+		expect(percentile).to.equal(3)
+	})
+
+	it('does not modify given array', () => {
+		const givenArray = [3, 2, 1]
+
+		computePercentile(givenArray, 100)
+
+		expect(givenArray).to.deep.equal([3, 2, 1])
+	})
+})
