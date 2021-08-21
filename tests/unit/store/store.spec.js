7b3ddd0807f10d7de1e1c43bb046990903125601
Ubald <ubald.de-bellabre@external.total.com>
2021-08-21 15:21:18 +0200

    Add basic test on the store

---
+++ tests/unit/store/store.spec.js
@@ -0,0 +1,8 @@
+import {expect} from "chai"
+import {store} from '@/store'
+
+describe('Store', () => {
+    it('is empty by default', () => {
+        expect(store.state).to.deep.equals({})
+    })
+})
