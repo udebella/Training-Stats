3ec0ba8b99c28a17a98d4ec7cb19291df4ccabc2
Ubald <ubald.de-bellabre@external.total.com>
2021-08-25 01:11:04 +0200

    Return new series when cancelling last value

--- src/domain/serie.js
+++ src/domain/serie.js
@@ -1,16 +1,20 @@
 import {computePercentile} from "../utils/percentiles"
 
-export const createSeries = ({ values = []} = {}) => {
+export const createSeries = ({values = []} = {}) => {
 	const tries = [...values]
 	const addTry = (value) => {
 		tries.push(value)
+		return createSeries({values: tries})
+	}
+	const cancelLast = () => {
+		tries.pop()
 		return createSeries({values: tries})
 	}
 	return {
 		numberTries: () => tries.length,
 		score: () => tries.reduce((score, value) => score + value, 0),
 		addTry,
-		cancelLast: () => tries.pop(),
+		cancelLast,
 		percentiles: () => [
 			computePercentile(tries, 0),
 			computePercentile(tries, 25),
