ee9653e740f418da9354493f1d560a6fb56f197a
Ubald <ubald.de-bellabre@external.total.com>
2021-08-25 01:10:23 +0200

    Return new series when adding values

--- src/domain/serie.js
+++ src/domain/serie.js
@@ -2,10 +2,14 @@ import {computePercentile} from "../utils/percentiles"
 
 export const createSeries = ({ values = []} = {}) => {
 	const tries = [...values]
+	const addTry = (value) => {
+		tries.push(value)
+		return createSeries({values: tries})
+	}
 	return {
 		numberTries: () => tries.length,
 		score: () => tries.reduce((score, value) => score + value, 0),
-		addTry: (value) => tries.push(value),
+		addTry,
 		cancelLast: () => tries.pop(),
 		percentiles: () => [
 			computePercentile(tries, 0),
